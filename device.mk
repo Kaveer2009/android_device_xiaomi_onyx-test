3389eebe043e415e51de715229987ba1b286155f
Zarathos30 <vale.rigni@gmail.com>
2026-03-18 17:53:55 +0100

    onyx: Increase Dalvik heap limits for 12GB RAM
    Raise heapgrowthlimit from 256m to 384m and heapsize from
    512m to 768m. Reduces GC pressure for heavy apps on a device
    with plenty of available memory.
    
    Signed-off-by: Zarathos30 <vale.rigni@gmail.com>

--- device.mk
+++ device.mk
@@ -134,8 +134,8 @@ PRODUCT_COPY_FILES += \
 # Dalvik
 PRODUCT_VENDOR_PROPERTIES += \
     dalvik.vm.heapstartsize=24m \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapsize=512m \
+    dalvik.vm.heapgrowthlimit=384m \
+    dalvik.vm.heapsize=768m \
     dalvik.vm.heaptargetutilization=0.46 \
     dalvik.vm.heapminfree=8m \
     dalvik.vm.heapmaxfree=48m
