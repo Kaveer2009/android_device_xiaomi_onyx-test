39b8329a21720b50526c65579e92c3a4bf630e60
zenin1504 <kayozzprojects@yandex.com>
2026-03-30 15:47:05 +0800

    onyx: Reformat Axion Flags
    Change-Id: Id85864c336ba4fb197e5c3871f04d143
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- lineage_onyx.mk
+++ lineage_onyx.mk
@@ -21,3 +21,22 @@ PRODUCT_MANUFACTURER := Xiaomi
 BuildFingerprint=POCO/onyx_global/onyx:16/BP2A.250605.031.A3/OS3.0.6.0.WOLMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Axion Device Configuration
+AXION_MAINTAINER := Zarathos_Ghost_Rider
+AXION_PROCESSOR := SM8735
+
+# Camera Info
+AXION_CAMERA_REAR_INFO := 50,8
+AXION_CAMERA_FRONT_INFO := 20
+
+# Graphics & Display
+TARGET_ENABLE_BLUR := true
+TARGET_SUPPORTED_REFRESH_RATES := 60,90,120
+HBM_SUPPORTED := true
+HBM_NODE := /data/vendor/display/hbm_mode
+
+# Features & Performance
+TARGET_INCLUDE_AXFX := true
+PERF_GOV_SUPPORTED := true
+PERF_DEFAULT_GOV := walt
