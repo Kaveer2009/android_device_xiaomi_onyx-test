b3380684f0dd162c9210e906fdde3ebbd23d0ecd
zenin1504 <kayozzprojects@yandex.com>
2026-03-30 15:47:05 +0800

    onyx: Reformat Axion Flags
    Change-Id: Id85864c336ba4fb197e5c3871f04d143
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- device.mk
+++ device.mk
@@ -541,21 +541,3 @@ PRODUCT_COPY_FILES += \
 
 # Vendor
 $(call inherit-product, vendor/xiaomi/onyx/onyx-vendor.mk)
-
-#Axion Stuff
-TARGET_INCLUDES_LOS_PREBUILTS := true
-TARGET_ENABLE_BLUR := true
-AXION_CAMERA_REAR_INFO := 50,8
-AXION_CAMERA_FRONT_INFO := 20
-AXION_MAINTAINER := Zarathos_Ghost_Rider
-AXION_PROCESSOR := SM8735
-LINEAGE_BUILDTYPE := OFFICIAL
-
-#Axion Performance Mode
-PERF_GOV_SUPPORTED := true
-PERF_DEFAULT_GOV := walt
-GPU_FREQS_PATH := /sys/class/kgsl/kgsl-3d0/devfreq/available_frequencies
-GPU_MIN_FREQ_PATH := /sys/class/kgsl/kgsl-3d0/devfreq/min_freq
-
-# Display refresh rates
-TARGET_SUPPORTED_REFRESH_RATES := 60,90,120
