d3c09fecbb8aac360c64367d51179ceabe4015c0
Zarathos30 <vale.rigni@gmail.com>
2026-05-12 17:27:11 +0200

    onyx: Update vendorsetup.sh
    Signed-off-by: Zarathos30 <vale.rigni@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,11 +3,11 @@
 # Hardware xiaomi (fresh clone)
 echo "Cloning hardware xiaomi source..."
 rm -rf hardware/xiaomi
-git clone -b lineage-23.2 https://github.com/sm8635-dev/hardware_xiaomi.git hardware/xiaomi
+git clone -b lineage-23.2 https://github.com/AxionAOSP-devices/android_hardware_xiaomi.git hardware/xiaomi
 
 # Kernel source (fresh clone)
 echo "Cloning kernel source tree..."
 rm -rf device/xiaomi/onyx-kernel
-git clone -b axion https://github.com/AxionAOSP-devices/android_kernel_xiaomi_onyx.git device/xiaomi/onyx-kernel
+git clone -b lineage-23.2 https://github.com/AxionAOSP-devices/android_kernel_xiaomi_onyx.git device/xiaomi/onyx-kernel
 
 echo "vendorsetup.sh execution complete."
